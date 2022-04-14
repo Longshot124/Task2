0cada552e907dabb069c3e01028843cbd7afee82
kamra <kamra@LAPTOP-M49P7DNF>
2022-04-14 14:42:15 +0400

    Second commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Reqem daxil edin ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            while (n != 0)
+            {
+                s = s + n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine("Reqemlerin cemi: {0}", s);
+            
+        }
+    }
+}
